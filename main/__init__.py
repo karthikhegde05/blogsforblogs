df09155b0f4febe672831848dca58ce97ad37d30
karthikhegde05 <karthikhegde05@gmail.com>
2021-07-03 12:59:09 +0530

    final commit

--- main/__init__.py
+++ main/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config.from_object("config.Config")
+app.config.from_object("config.ProductionConfig")
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
